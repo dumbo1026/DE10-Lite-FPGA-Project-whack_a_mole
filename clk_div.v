a5e2aaee1ba69d201cbb60bfffc81d8b5291c0ef
C00p1r <3660a3@gmail.com>
2025-12-26 18:39:22 +0800

    Add files via upload

---
+++ clk_div.v
@@ -0,0 +1,31 @@
+// 25000000 1 sec
+
+module clock_divider(
+	input clk,
+	input rst,
+	input [31:0] TimeExpire,
+	output reg div_clk);
+reg [31:0] counter;
+
+always@(posedge clk or negedge rst)
+begin
+	if (!rst) // reset when rst is 0
+	begin
+		counter <= 0;
+		div_clk <= 0;
+	end
+	else
+	begin
+		if (counter == TimeExpire)
+		begin
+			counter <= 0;
+			div_clk <= ~div_clk;
+		end
+		else
+		begin
+			counter <= counter + 1;
+		end
+	end
+end
+
+endmodule
